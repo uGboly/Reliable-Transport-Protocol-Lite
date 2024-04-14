059ac496c627a928ffe8ae8ea13cb3e6b61f046b
Zhang Lin <zlin@bupt.edu.cn>
2024-04-14 13:36:09 +0800

    整理main函数

--- receiver.py
+++ receiver.py
@@ -1,11 +1,10 @@
 import socket
 import sys
-import threading
-from STPSegment import STPSegment, DATA, ACK, SYN, FIN
-from receiver_utils import ReceiverLogger
+from STPSegment import STPSegment, ACK, SYN, FIN
+from receiver_utils import ReceiverLogger, WindowManager
 
 
-class STPReceiver:
+class Receiver:
     def __init__(self, receiver_port, sender_port, file_to_save, max_win):
         self.receiver_port = receiver_port
         self.sender_port = sender_port
@@ -15,12 +14,10 @@ class STPReceiver:
         self.expected_seqno = 0
         self.rcv_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.rcv_socket.bind(('', receiver_port))
-        self.original_data_received = 0
-        self.original_segments_received = 0
-        self.dup_data_segments_received = 0
+        self.window_manager = WindowManager(self, file_to_save)
+        self.logger = ReceiverLogger()
         self.total_ack_segments_sent = 0
         self.rcv_socket.settimeout(2.0)
-        self.logger = ReceiverLogger()
 
     def send_ack(self, seqno, address):
         ack = STPSegment(ACK, seqno)
@@ -35,90 +32,6 @@ class STPReceiver:
         return segment, origination
 
 
-    def start(self):
-        self.handle_syn()
-        self.receive_data()
-
-    def handle_syn(self):
-        # 等待并处理SYN
-        while True:
-            try:
-                segment, origination = self.receive_segment()
-                if segment.type == SYN:
-                    self.expected_seqno = (segment.seqno + 1) % (2 ** 16 - 1)
-                    self.init_seqno = (segment.seqno + 1) % (2 ** 16 - 1)
-                    self.send_ack(self.expected_seqno, origination)
-                    break
-            except socket.timeout:
-                continue
-
-    def receive_data(self):
-        buffer = {}  # 使用字典作为缓冲区，键为序列号，值为数据
-        with open(self.file_to_save, 'wb') as file:
-            while True:
-                try:
-                    segment, origination = self.receive_segment()
-
-                    if segment.type == SYN:
-                        self.send_ack(self.init_seqno, origination)
-
-                    elif segment.type == DATA:
-                        # 如果数据段按序到达，直接写入文件，并检查缓冲区中是否有连续的后续数据
-                        if segment.seqno == self.expected_seqno:
-                            self.original_data_received += len(segment.data)
-                            self.original_segments_received += 1
-                            file.write(segment.data)
-                            self.expected_seqno = (
-                                self.expected_seqno + len(segment.data)) % (2 ** 16 - 1)
-
-                            # 检查并写入缓冲区中按序的数据
-                            while self.expected_seqno in buffer:
-                                data = buffer.pop(self.expected_seqno)
-                                file.write(data)
-                                self.expected_seqno = (
-                                    self.expected_seqno + len(data)) % (2 ** 16 - 1)
-                        elif segment.seqno > self.expected_seqno:  # 到达了乱序数据
-                            if segment.seqno in buffer:
-                                self.dup_data_segments_received += 1
-                            else:
-                                self.original_data_received += len(
-                                    segment.data)
-                                self.original_segments_received += 1
-                                buffer[segment.seqno] = segment.data
-
-                        self.send_ack(self.expected_seqno, origination)
-
-                    elif segment.type == FIN:
-                        self.logger.log("rcv", segment)
-                        self.handle_fin(origination)
-                        break
-                except socket.timeout:
-                    continue
-
-    def handle_fin(self, origination):
-        self.send_ack((self.expected_seqno + 1) %
-                      (2 ** 16 - 1), origination)
-
-        # 定义处理FIN重传的线程函数
-        def handle_fin_retransmissions():
-            try:
-                while True:
-                    segment, _ = self.receive_segment()
-
-                    if segment.type == FIN:
-                        # 对于重传的FIN，再次发送ACK
-                        self.send_ack((self.expected_seqno + 1) %
-                                      (2 ** 16 - 1), origination)
-            except socket.timeout:
-                pass
-
-        # 启动处理FIN重传的线程
-        fin_thread = threading.Thread(target=handle_fin_retransmissions)
-        fin_thread.start()
-        fin_thread.join()
-        self.logger.log_statatics(self)
-
-
 if __name__ == '__main__':
     if len(sys.argv) != 5:
         print(
@@ -130,9 +43,49 @@ if __name__ == '__main__':
     txt_file_received = sys.argv[3]
     max_win = int(sys.argv[4])
 
-    with open("receiver_log.txt", "w") as log_file:
-        pass
+    receiver = Receiver(receiver_port, sender_port,
+                        txt_file_received, max_win)
 
-    receiver = STPReceiver(receiver_port, sender_port,
-                           txt_file_received, max_win)
-    receiver.start()
+    while True:
+        try:
+            segment, origination = receiver.receive_segment()
+            if segment.type == SYN:
+                receiver.init_seqno = (segment.seqno + 1) % (2 ** 16 - 1)
+                receiver.expected_seqno = receiver.init_seqno
+                receiver.send_ack(receiver.expected_seqno, origination)
+                break
+        except socket.timeout:
+            continue
+
+    while True:
+        try:
+            segment, origination = receiver.receive_segment()
+
+            match segment.type:
+                case 3:
+                    break
+                case 2:
+                    receiver.send_ack(receiver.init_seqno, origination)
+                case 0:
+                    receiver.window_manager.handle_segment(segment)
+                    receiver.send_ack(receiver.expected_seqno, origination)
+                case _:
+                    pass
+
+        except socket.timeout:
+            continue
+
+
+    receiver.send_ack((receiver.expected_seqno + 1) %
+                      (2 ** 16 - 1), origination)
+
+    try:
+        while True:
+            segment, _ = receiver.receive_segment()
+
+            if segment.type == FIN:
+                receiver.send_ack((receiver.expected_seqno + 1) %
+                                  (2 ** 16 - 1), origination)
+    except socket.timeout:
+        pass
+    receiver.logger.log_statatics(receiver)
--- receiver_utils.py
+++ receiver_utils.py
@@ -1,6 +1,4 @@
 import time
-import socket
-from STPSegment import STPSegment, ACK, DATA, SYN, FIN
 
 class ReceiverLogger:
     def __init__(self, log_file_path="receiver_log.txt"):
@@ -23,7 +21,43 @@ class ReceiverLogger:
 
     def log_statatics(self, stat):
         with open(self.log_file_path, "a") as log_file:
-            log_file.write(f"Original data received: {stat.original_data_received}\n")
-            log_file.write(f"Original segments received: {stat.original_segments_received}\n")
-            log_file.write(f"Dup data segments received: {stat.dup_data_segments_received}\n")
-            log_file.write(f"Dup ack segments sent: {stat.total_ack_segments_sent - stat.original_segments_received - 2}\n")
+            log_file.write(f"Original data received: {stat.window_manager.original_data_received}\n")
+            log_file.write(f"Original segments received: {stat.window_manager.original_segments_received}\n")
+            log_file.write(f"Dup data segments received: {stat.window_manager.dup_data_segments_received}\n")
+            log_file.write(f"Dup ack segments sent: {stat.total_ack_segments_sent - stat.window_manager.original_segments_received - 2}\n")
+
+
+class WindowManager:
+    def __init__(self, receiver, file_path):
+        self.file_path = file_path
+        self.win = {}
+        self.receiver = receiver
+        self.original_data_received = 0
+        self.original_segments_received = 0
+        self.dup_data_segments_received = 0
+
+    def handle_segment(self, segment):
+        with open(self.file_path, 'ab') as file:
+            if segment.seqno == self.receiver.expected_seqno:  # In-order segment
+                self.original_data_received += len(segment.data)
+                self.original_segments_received += 1
+                file.write(segment.data)
+                self.update_expected_seqno(len(segment.data))
+
+                # Write any buffered, now in-order, segments
+                while self.receiver.expected_seqno in self.win:
+                    data = self.win.pop(self.receiver.expected_seqno)
+                    file.write(data)
+                    self.update_expected_seqno(len(data))
+
+            elif segment.seqno > self.receiver.expected_seqno:  # Out-of-order segment
+                if segment.seqno not in self.win:
+                    self.win[segment.seqno] = segment.data
+                else:
+                    self.dup_data_segments_received += 1
+
+    def update_expected_seqno(self, data_length):
+        self.receiver.expected_seqno = (self.receiver.expected_seqno + data_length) % (2 ** 16 - 1)
+
+    def get_stats(self):
+        return (self.original_data_received, self.original_segments_received, self.dup_data_segments_received)
