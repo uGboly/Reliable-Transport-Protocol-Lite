c04dff706ffd8af39fc6d8b736a934006810365e
Zhang Lin <zlin@bupt.edu.cn>
2024-04-16 17:58:56 +0800

    Rename control block

--- sender.py
+++ sender.py
@@ -6,9 +6,10 @@ import threading
 from sender_components.segment_handler import snd_seg, rcv_seg
 from sender_components.logger import ActionLogger
 
-class STPControlBlock:
+
+class STPSender:
     def __init__(self):
-        self.state = "CLOSED"
+        self.state = "CLOSE"
         self.isn = random.randint(0, 65535)
         self.seqno = self.isn + 1
         self.lock = threading.Lock()
@@ -17,6 +18,7 @@ class STPControlBlock:
         self.parameters = dict(rto=rto, flp=flp, rlp=rlp)
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind(('', sender_port))
+        self.sock.settimeout(rto / 1000.0)
         self.dest = ('localhost', receiver_port)
         self.unack_segments = []
         self.timer = None
@@ -24,109 +26,102 @@ class STPControlBlock:
         self.action_logger = ActionLogger()
 
 
-
-def establish_connection(control_block):
-
-    snd_seg(control_block, 2, control_block.isn)
-
-    control_block.sock.settimeout(control_block.rto / 1000.0)
+def establish_connection(stp_sender):
+    snd_seg(stp_sender, 2, stp_sender.isn)
 
     while True:
         try:
-            ack_seqno = rcv_seg(control_block)
+            ack_seqno = rcv_seg(stp_sender)
 
-            if ack_seqno == control_block.isn + 1:
-                control_block.state = "ESTABLISHED"
+            if ack_seqno == stp_sender.isn + 1:
+                stp_sender.state = "ESTABLISHED"
                 break
         except socket.timeout:
-            snd_seg(control_block, 2, control_block.isn)
-        except AttributeError:
-            continue
+            snd_seg(stp_sender, 2, stp_sender.isn)
 
 
-def ack_receiver(control_block):
+def ack_receiver(stp_sender):
     while True:
 
         try:
-            ack_seqno = rcv_seg(control_block)
+            ack_seqno = rcv_seg(stp_sender)
 
-            with control_block.lock:
-                if control_block.state == "FIN_WAIT" and ack_seqno == control_block.seqno + 1:
-                    control_block.state = "CLOSE"
+            with stp_sender.lock:
+                if stp_sender.state == "FIN_WAIT" and ack_seqno == stp_sender.seqno + 1:
+                    stp_sender.state = "CLOSE"
                     break
 
-                elif ack_seqno in [(seg[0] + len(seg[1])) % 2 ** 16 for seg in control_block.unack_segments]:
+                elif ack_seqno in [(seg[0] + len(seg[1])) % 2 ** 16 for seg in stp_sender.unack_segments]:
 
                     index_ack_seg = [i for i, seg in enumerate(
-                        control_block.unack_segments) if (seg[0] + len(seg[1])) % 2 ** 16 == ack_seqno][0]
-                    for seg in control_block.unack_segments[:index_ack_seg + 1]:
-                        control_block.action_logger.original_data_acked += len(seg[1])
+                        stp_sender.unack_segments) if (seg[0] + len(seg[1])) % 2 ** 16 == ack_seqno][0]
+                    for seg in stp_sender.unack_segments[:index_ack_seg + 1]:
+                        stp_sender.action_logger.original_data_acked += len(
+                            seg[1])
 
-                    control_block.unack_segments = control_block.unack_segments[index_ack_seg + 1:]
+                    stp_sender.unack_segments = stp_sender.unack_segments[index_ack_seg + 1:]
 
-                    if control_block.unack_segments:
-                        control_block.timer = time.time() * 1000 + control_block.rto
+                    if stp_sender.unack_segments:
+                        stp_sender.timer = time.time() * 1000 + stp_sender.rto
                     else:
-                        control_block.timer = None
+                        stp_sender.timer = None
 
-                    control_block.dup_ack_count = {}
+                    stp_sender.dup_ack_count = {}
 
                 else:
 
-                    control_block.action_logger.dup_acks_received += 1
-                    if ack_seqno in control_block.dup_ack_count:
-                        control_block.dup_ack_count[ack_seqno] += 1
+                    stp_sender.action_logger.dup_acks_received += 1
+                    if ack_seqno in stp_sender.dup_ack_count:
+                        stp_sender.dup_ack_count[ack_seqno] += 1
                     else:
-                        control_block.dup_ack_count[ack_seqno] = 1
+                        stp_sender.dup_ack_count[ack_seqno] = 1
 
-                    if control_block.dup_ack_count[ack_seqno] == 3:
-                        oldest_unack_segment = control_block.unack_segments[0]
+                    if stp_sender.dup_ack_count[ack_seqno] == 3:
+                        oldest_unack_segment = stp_sender.unack_segments[0]
                         snd_seg(
-                            control_block, 0, oldest_unack_segment[0], oldest_unack_segment[1], True)
+                            stp_sender, 0, oldest_unack_segment[0], oldest_unack_segment[1], True)
 
-                        control_block.timer = time.time() * 1000 + control_block.rto
+                        stp_sender.timer = time.time() * 1000 + stp_sender.rto
 
         except socket.timeout:
             continue
-        except AttributeError:
-            continue
 
 
-def timer_thread(control_block):
+def timer_thread(stp_sender):
     while True:
-        with control_block.lock:
-            if control_block.state == "CLOSE":
+        with stp_sender.lock:
+            if stp_sender.state == "CLOSE":
 
                 break
-            if control_block.timer is not None:
+            if stp_sender.timer is not None:
                 current_time = time.time() * 1000
-                if current_time >= control_block.timer:
+                if current_time >= stp_sender.timer:
 
-                    if control_block.unack_segments:
-                        oldest_unack_segment = control_block.unack_segments[0]
+                    if stp_sender.unack_segments:
+                        oldest_unack_segment = stp_sender.unack_segments[0]
                         snd_seg(
-                            control_block, 0, oldest_unack_segment[0], oldest_unack_segment[1], True)
+                            stp_sender, 0, oldest_unack_segment[0], oldest_unack_segment[1], True)
 
-                        control_block.timer = current_time + control_block.rto
+                        stp_sender.timer = current_time + stp_sender.rto
 
-                        control_block.dup_ack_count = {}
-                    elif control_block.state == "FIN_WAIT":
+                        stp_sender.dup_ack_count = {}
+                    elif stp_sender.state == "FIN_WAIT":
 
-                        snd_seg(control_block, 3,
-                                     control_block.seqno, b'', True)
-                        control_block.timer = current_time + control_block.rto
+                        snd_seg(stp_sender, 3,
+                                stp_sender.seqno, b'', True)
+                        stp_sender.timer = current_time + stp_sender.rto
 
 
-def send_file(filename, control_block):
+def send_file(filename, stp_sender):
     with open(filename, 'rb') as file:
         file_data = file.read()
         total_length = len(file_data)
         sent_length = 0
 
-        while sent_length < total_length or control_block.unack_segments:
-            with control_block.lock:
-                window_space = control_block.window_size - \
-                    (len(control_block.unack_segments) * 1000)
+        while sent_length < total_length or stp_sender.unack_segments:
+            with stp_sender.lock:
+                window_space = stp_sender.window_size - \
+                    (len(stp_sender.unack_segments) * 1000)
 
                 if window_space > 0 and sent_length < total_length:
 
@@ -134,31 +129,29 @@ def send_file(filename, control_block):
                                        sent_length, window_space)
                     segment_data = file_data[sent_length:sent_length+segment_size]
 
-                    snd_seg(control_block, 0,
-                                 control_block.seqno, segment_data)
+                    snd_seg(stp_sender, 0,
+                            stp_sender.seqno, segment_data)
 
-                    if not control_block.unack_segments:
-                        control_block.timer = time.time() * 1000 + control_block.rto
-                    control_block.unack_segments.append(
-                        [control_block.seqno, segment_data])
-                    control_block.seqno = (
-                        control_block.seqno + segment_size) % (2 ** 16)
+                    if not stp_sender.unack_segments:
+                        stp_sender.timer = time.time() * 1000 + stp_sender.rto
+                    stp_sender.unack_segments.append(
+                        [stp_sender.seqno, segment_data])
+                    stp_sender.seqno = (
+                        stp_sender.seqno + segment_size) % (2 ** 16)
 
                     sent_length += segment_size
 
 
-def close_connection(control_block):
+def close_connection(stp_sender):
     while True:
-        with control_block.lock:
-            if not control_block.unack_segments:
+        with stp_sender.lock:
+            if not stp_sender.unack_segments:
 
-                control_block.state = "FIN_WAIT"
+                stp_sender.state = "FIN_WAIT"
 
-                snd_seg(control_block, 3, control_block.seqno)
-                control_block.timer = time.time() * 1000 + control_block.rto
+                snd_seg(stp_sender, 3, stp_sender.seqno)
+                stp_sender.timer = time.time() * 1000 + stp_sender.rto
                 break
-
-
 
 
 if __name__ == '__main__':
@@ -174,32 +167,30 @@ if __name__ == '__main__':
     flp = float(sys.argv[6])
     rlp = float(sys.argv[7])
 
-
     random.seed()
 
-    control_block = STPControlBlock()
+    stp_sender = STPSender()
 
-    establish_connection(control_block)
+    establish_connection(stp_sender)
 
-    if control_block.state != "ESTABLISHED":
+    if stp_sender.state != "ESTABLISHED":
         print("Failed to establish connection.")
         sys.exit(1)
 
     ack_thread = threading.Thread(
-        target=ack_receiver, args=[control_block])
+        target=ack_receiver, args=[stp_sender])
     timer_thread = threading.Thread(
-        target=timer_thread, args=[control_block])
+        target=timer_thread, args=[stp_sender])
     ack_thread.start()
     timer_thread.start()
 
-    send_file(txt_file_to_send, control_block)
+    send_file(txt_file_to_send, stp_sender)
 
-    close_connection(control_block)
+    close_connection(stp_sender)
 
     ack_thread.join()
     timer_thread.join()
 
-    control_block.sock.close()
+    stp_sender.sock.close()
 
-    with control_block.lock:
-        control_block.action_logger.summary()
+    stp_sender.action_logger.summary()
--- sender_components/segment_handler.py
+++ sender_components/segment_handler.py
@@ -2,40 +2,40 @@ import random
 import struct
 
 
-def snd_seg(control_block, type, seqno, data=b'', old_seg=False):
-    len_data = len(data) if type == 0 else 0
+def snd_seg(sender, type, seqno, data=b'', old_seg=False):
+    len_data = len(data) if not type else 0
 
-    if random.random() < control_block.parameters['flp']:
-        if type == 0:
-            control_block.action_logger.data_segments_dropped += 1
+    if random.random() < sender.parameters['flp']:
+        if not type:
+            sender.action_logger.data_segments_dropped += 1
             if not old_seg:
-                control_block.action_logger.original_data_sent += len_data
-                control_block.action_logger.original_segments_sent += 1
+                sender.action_logger.original_data_sent += len_data
+                sender.action_logger.original_segments_sent += 1
 
-        control_block.action_logger.action_logging("drp", type,
+        sender.action_logger.action_logging("drp", type,
                   seqno, len_data)
     else:
         sent_data = struct.pack('!HH', type, seqno) + data
-        control_block.sock.sendto(sent_data, control_block.dest)
-        control_block.action_logger.action_logging("snd", type,
+        sender.sock.sendto(sent_data, sender.dest)
+        sender.action_logger.action_logging("snd", type,
                   seqno, len_data)
 
-        if type == 0:
+        if not type:
             if old_seg:
-                control_block.action_logger.retransmitted_segments += 1
+                sender.action_logger.retransmitted_segments += 1
             else:
-                control_block.action_logger.original_data_sent += len_data
-                control_block.action_logger.original_segments_sent += 1
+                sender.action_logger.original_data_sent += len_data
+                sender.action_logger.original_segments_sent += 1
 
 
-def rcv_seg(control_block):
-    response, _ = control_block.sock.recvfrom(1024)
+def rcv_seg(sender):
+    response, _ = sender.sock.recvfrom(1024)
     _, ack_seqno = struct.unpack('!HH', response)
 
-    if random.random() < control_block.parameters['rlp']:
-        control_block.action_logger.ack_segments_dropped += 1
-        control_block.action_logger.action_logging("drp", 1, ack_seqno)
+    if random.random() < sender.parameters['rlp']:
+        sender.action_logger.ack_segments_dropped += 1
+        sender.action_logger.action_logging("drp", 1, ack_seqno)
         return
     else:
-        control_block.action_logger.action_logging("rcv", 1, ack_seqno)
+        sender.action_logger.action_logging("rcv", 1, ack_seqno)
         return ack_seqno
