834d79b3432f79919c0c9c907ac95263a22033d4
Zhang Lin <zlin@bupt.edu.cn>
2024-04-16 23:35:59 +0800

    重构sender的发送接收函数

--- sender_components/segment_handler.py
+++ sender_components/segment_handler.py
@@ -2,35 +2,40 @@ import random
 import struct
 
 
-def snd_seg(sender, type, seqno, data=b'', old_seg=False):
-    len_data = len(data) if not type else 0
+def snd_seg(sender, seg_type, seqno, data=b'', is_old_segment=False):
+    # Determine length of data to send based on segment type
+    data_length = 0 if seg_type else len(data)
+
+    def log_action(action, length=data_length):
+        """Helper function to log actions."""
+        sender.action_logger.action_logging(action, seg_type, seqno, length)
+        if not seg_type:
+            if not is_old_segment:
+                sender.action_logger.original_data_sent += length
+                sender.action_logger.original_segments_sent += 1
+            else:
+                sender.action_logger.retransmitted_segments += 1
+                
 
     if random.random() < sender.parameters['flp']:
-        if not type:
+        # Log data segment drop
+        log_action("drp")
+        if seg_type:
+            pass
+        else:
             sender.action_logger.data_segments_dropped += 1
-            if not old_seg:
-                sender.action_logger.original_data_sent += len_data
-                sender.action_logger.original_segments_sent += 1
-
-        sender.action_logger.action_logging("drp", type,
-                  seqno, len_data)
     else:
-        sent_data = struct.pack('!HH', type, seqno) + data
+        # Prepare and send data
+        sent_data = struct.pack('!HH', seg_type, seqno) + data
         sender.sock.sendto(sent_data, sender.dest)
-        sender.action_logger.action_logging("snd", type,
-                  seqno, len_data)
-
-        if not type:
-            if old_seg:
-                sender.action_logger.retransmitted_segments += 1
-            else:
-                sender.action_logger.original_data_sent += len_data
-                sender.action_logger.original_segments_sent += 1
+        # Log data sent
+        log_action("snd")
 
 
 def rcv_seg(sender):
-    response, _ = sender.sock.recvfrom(1024)
-    _, ack_seqno = struct.unpack('!HH', response)
+    ack = sender.sock.recvfrom(1024)[0]
+
+    ack_seqno = struct.unpack('!HH', ack)[1]
 
     if random.random() < sender.parameters['rlp']:
         sender.action_logger.ack_segments_dropped += 1
